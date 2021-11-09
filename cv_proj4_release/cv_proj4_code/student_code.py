4dcfe695d0a475863b52d2cc2d3a4d3e592084e1
AaronLopes <aaronlopes@me.com>
2021-11-09 01:22:40 -0500

    4.2 finish

--- cv_proj4_release/cv_proj4_code/student_code.py
+++ cv_proj4_release/cv_proj4_code/student_code.py
@@ -244,8 +244,26 @@ def nms_maxpool_pytorch(R: torch.tensor, k: int, ksize: int) -> Tuple[torch.tens
     """
     median = torch.median(R)
     R[R < median] = 0
-    m = torch.nn.MaxPool2d(ksize)
-    maxpooled_R = m(R)
+    pool = nn.MaxPool2d(ksize, 1, padding=ksize // 2)
+    R = R[None, None, :]
+    pooled_R = pool(R)
+    pooled_R = pooled_R[0][0]
+    binary_R = []
+    for r in range(pooled_R.shape[0]):
+        bin_row = []
+        for c in range(pooled_R.shape[1]):
+            if pooled_R[r][c] == R[0][0][r][c]:
+                bin_row.append(1)
+            else:
+                bin_row.append(0)
+        binary_R.append(bin_row)
+    binary_R = torch.tensor(binary_R)
+    masked_R = R * binary_R
+    o, i = torch.topk(masked_R[0][0].flatten(), k)
+    indices = np.array(np.unravel_index(i.numpy(), masked_R[0][0].shape)).T
+    x = torch.tensor(indices[:, 0])
+    y = torch.tensor(indices[:, 1])
+    return x, y, o
 
 
 # TODO 5.1
